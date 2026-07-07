5353c6621c1f6abb6a741e31984bd063a7ab0c08
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-07-07 09:06:11 +0200

    Added reset button

--- src/components/CounterCard.tsx
+++ src/components/CounterCard.tsx
@@ -6,11 +6,16 @@ const CounterCard = () => {
   function incrementCounter() {
     setCounter((prev) => prev + 1);
   }
+
+  function reset() {
+    setCounter(0);
+  }
   return (
     <div>
       <h1>React Counter</h1>
       <p>Count: {counter}</p>
       <button onClick={incrementCounter}>Increment</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
